d58355143c19fb85f2d5676f2e9b63b8d4867a62
Sponteuh <sponte2556@gmail.com>
2026-01-23 22:05:50 +0100

    feat: finish import music ig

--- scripts/system/MusicManager.cs
+++ scripts/system/MusicManager.cs
@@ -29,6 +29,8 @@ public partial class MusicManager : Node
 	{
 		_playlist.Add(GD.Load<AudioStream>("res://assets/musics/background/GroovyDrink.mp3"));
 		_playlist.Add(GD.Load<AudioStream>("res://assets/musics/background/SeasideDrink.mp3"));
+		_playlist.Add(GD.Load<AudioStream>("res://assets/musics/background/Jet27Drink.mp3"));
+		_playlist.Add(GD.Load<AudioStream>("res://assets/musics/background/SeriousDrink.mp3"));
 		GD.Print($"🎵 [{GetInstanceId()}] Loaded {_playlist.Count} tracks");
 	}
 
