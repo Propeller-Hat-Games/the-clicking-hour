a782540a06d490065be006d49427cc33a43db3fc
Hiiwatari <raccoon.stunting@gmail.com>
2026-01-23 12:56:57 +0100

    fix: glass on top left removed

--- scripts/system/GameManager.cs
+++ scripts/system/GameManager.cs
@@ -165,7 +165,6 @@ public partial class GameManager : Node2D {
 	public override void _Ready() {
 		var glassScene = GD.Load<PackedScene>("res://scenes/glass.tscn");
 		var glassInstance = glassScene.Instantiate<Glass>();
-		AddChild(glassInstance);
 
 		glassSprites = glassInstance.GetSprites();
 		
