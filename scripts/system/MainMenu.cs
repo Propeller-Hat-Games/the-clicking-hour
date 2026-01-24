08c71405fba813d06734ef906b97fb0c904e824f
Baptiste May <baptistemay@ik.me>
2026-01-24 03:37:23 +0100

    feat: add credit meny

--- scripts/system/MainMenu.cs
+++ scripts/system/MainMenu.cs
@@ -5,6 +5,11 @@ public partial class MainMenu : Control {
 	[Signal]
 	public delegate void GameStartedEventHandler();
 
+	[Export]
+	public PackedScene CreditMenuScene { get; set; }
+
+	private Control _creditMenuInstance;
+
 	public override void _Ready()
 	{
 		var window = GetNodeOrNull<Control>("CanvasLayer/Window");
@@ -22,6 +27,22 @@ public partial class MainMenu : Control {
 
 	public void _on_play_button_pressed() {
 		EmitSignal(SignalName.GameStarted);
+	}
+
+	public void _on_credits_button_pressed() {
+		if (CreditMenuScene != null) {
+			var creditMenu = CreditMenuScene.Instantiate<CreditMenu>();
+			_creditMenuInstance = creditMenu;
+			AddChild(_creditMenuInstance);
+			creditMenu.CloseRequested += _on_credit_menu_closed;
+		}
+	}
+
+	private void _on_credit_menu_closed() {
+		if (_creditMenuInstance != null) {
+			_creditMenuInstance.QueueFree();
+			_creditMenuInstance = null;
+		}
 	}
 
 	public void _on_quit_button_pressed() {
