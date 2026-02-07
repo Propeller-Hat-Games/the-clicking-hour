47deebdb6e62201c0dc4738ddb1c38692ceb82fd
Flamasar <obsiflamme@gmail.com>
2026-02-07 21:10:38 +0100

    feat: multiclick sprites and integration

--- scripts/environment/entity/MultiClickEntity.cs
+++ scripts/environment/entity/MultiClickEntity.cs
@@ -6,39 +6,40 @@ using Godot;
 /// </summary>
 public partial class MultiClickEntity : Entity
 {
-    private float StunTimer { get; set; } = 0f;
-    private const float STUN_DURATION = 0.5f;
+	private float StunTimer { get; set; } = 0f;
+	private const float STUN_DURATION = 0.5f;
 
-    /// <summary>
-    /// Initializes the entity with a random number of hearts between 3 and 5.
-    /// </summary>
-    protected override void InitializeEntity()
-    {
-        Hearts = (int)GD.RandRange(3, 5);
-    }
+	/// <summary>
+	/// Initializes the entity with a random number of hearts between 3 and 5.
+	/// </summary>
+	protected override void InitializeEntity()
+	{
+		AnimPrefix = "multiclick";
+		Hearts = (int)GD.RandRange(3, 5);
+	}
 
-    /// <summary>
-    /// Sets the entity state to stunned when clicked.
-    /// </summary>
-    protected override void OnClicked()
-    {
-        CurrentState = EntityState.Stunned;
-        StunTimer = STUN_DURATION;
-    }
+	/// <summary>
+	/// Sets the entity state to stunned when clicked.
+	/// </summary>
+	protected override void OnClicked()
+	{
+		CurrentState = EntityState.Stunned;
+		StunTimer = STUN_DURATION;
+	}
 
-    /// <summary>
-    /// Processes the stun timer and returns the entity to walking state when finished.
-    /// </summary>
-    /// <param name="delta">Time since last frame.</param>
-    protected override void ProcessEntity(double delta)
-    {
-        if (CurrentState == EntityState.Stunned)
-        {
-            StunTimer -= (float)delta;
-            if (StunTimer <= 0)
-            {
-                CurrentState = EntityState.Walking;
-            }
-        }
-    }
+	/// <summary>
+	/// Processes the stun timer and returns the entity to walking state when finished.
+	/// </summary>
+	/// <param name="delta">Time since last frame.</param>
+	protected override void ProcessEntity(double delta)
+	{
+		if (CurrentState == EntityState.Stunned)
+		{
+			StunTimer -= (float)delta;
+			if (StunTimer <= 0)
+			{
+				CurrentState = EntityState.Walking;
+			}
+		}
+	}
 }
