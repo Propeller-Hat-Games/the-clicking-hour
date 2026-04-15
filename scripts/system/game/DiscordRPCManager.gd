14890ae278bbdf55cb8a84281c6e8d806291780a
Baptiste May <baptistemay@ik.me>
2026-04-15 22:10:11 +0200

    fix: correct timestamp

--- scripts/system/game/DiscordRPCManager.gd
+++ scripts/system/game/DiscordRPCManager.gd
@@ -3,19 +3,25 @@ extends Node
 ## Manages Discord Rich Presence integration.
 
 var _game: GameManager
+var _game_start_timestamp: int = 0
+var _pause_start_timestamp: int = 0
+var _is_paused: bool = false
 
 
 func init(p_game: GameManager) -> void:
 	_game = p_game
 	DiscordRPC.app_id = 1493718940147257365
 	DiscordRPC.large_image = "icon"
-	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system())
 	set_main_menu()
 
 
 func set_main_menu() -> void:
+	_game_start_timestamp = 0
+	_is_paused = false
 	DiscordRPC.details = "In the main menu"
 	DiscordRPC.state = ""
+	DiscordRPC.start_timestamp = 0
+	DiscordRPC.end_timestamp = 0
 	DiscordRPC.refresh()
 
 
@@ -33,18 +39,36 @@ func set_playing(wave: int, is_night_mode: bool) -> void:
 	DiscordRPC.details = "In game"
 	var mode_text = " (Night Mode)" if is_night_mode else ""
 	DiscordRPC.state = "Wave %d%s" % [wave, mode_text]
+
+	if _game_start_timestamp == 0:
+		_game_start_timestamp = int(Time.get_unix_time_from_system())
+
+	DiscordRPC.start_timestamp = _game_start_timestamp
+	DiscordRPC.end_timestamp = 0
 	DiscordRPC.refresh()
 
 
 func set_paused(is_paused: bool) -> void:
 	if is_paused:
+		if not _is_paused:
+			_pause_start_timestamp = int(Time.get_unix_time_from_system())
+			_is_paused = true
 		DiscordRPC.details = "Paused"
+		DiscordRPC.start_timestamp = 0
+		DiscordRPC.refresh()
 	else:
+		if _is_paused:
+			var pause_duration = int(Time.get_unix_time_from_system()) - _pause_start_timestamp
+			_game_start_timestamp += pause_duration
+			_is_paused = false
 		set_playing(_game.current_wave, _game.is_night_mode)
-	DiscordRPC.refresh()
 
 
 func set_game_over(wave: int) -> void:
 	DiscordRPC.details = "Game Over"
 	DiscordRPC.state = "Survived %d waves" % wave
+	DiscordRPC.start_timestamp = _game_start_timestamp
+	DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system())
+	_game_start_timestamp = 0
+	_is_paused = false
 	DiscordRPC.refresh()
