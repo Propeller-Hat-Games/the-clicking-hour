3f89806fe667b2cba70602c53582be135d5e3d4f
HugoLouis-Joseph <hugolouisjoseph7@gmail.com>
2026-04-28 15:51:23 +0200

    première implémentation des probabilités via une courbe, à revoir, pas très convainquant

--- scripts/environment/Glass.gd
+++ scripts/environment/Glass.gd
@@ -3,6 +3,13 @@ extends Node2D
 ## Manages a collection of glass sprites.
 
 @export var sprites: Dictionary = {}
+@export var probabilityCurve: Curve
+var amountOfApparitionsPerType : Dictionary = {}
+var sumOfApparitions : int = 0
+
+func _ready() -> void:
+	for type in sprites.keys():
+		amountOfApparitionsPerType[type] = 0
 
 ## Returns the dictionary of glass sprites.
 func get_sprites() -> Dictionary:
@@ -12,7 +19,15 @@ func get_sprites() -> Dictionary:
 ## Returns a random glass key from the available sprites.
 func get_random_glass() -> String:
 	var keys = sprites.keys()
-	if keys.size() == 0:
-		return ""
-	var random_index = randi() % keys.size()
-	return keys[random_index]
+	
+	var returnType = keys[-1]
+	for type in keys.slice(0,-1):
+		var quot : float = (1.0+amountOfApparitionsPerType[type])/(keys.size()+sumOfApparitions)
+		var probability : float = probabilityCurve.sample(quot)
+		if (randf() <= probability) :
+			returnType = type
+			break
+	
+	amountOfApparitionsPerType[returnType] += 1
+	sumOfApparitions += 1
+	return returnType
