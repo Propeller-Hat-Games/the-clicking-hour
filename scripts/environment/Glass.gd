6a6875369abf9e5d8ee33d775e38739a1f2f5cfc
HugoLouis-Joseph <hugolouisjoseph7@gmail.com>
2026-04-28 16:13:48 +0200

    chore: changed the way the probability worked, seems a bit better

--- scripts/environment/Glass.gd
+++ scripts/environment/Glass.gd
@@ -4,12 +4,9 @@ extends Node2D
 
 @export var sprites: Dictionary = {}
 @export var probabilityCurve: Curve
-var amountOfApparitionsPerType : Dictionary = {}
-var sumOfApparitions : int = 0
-
-func _ready() -> void:
-	for type in sprites.keys():
-		amountOfApparitionsPerType[type] = 0
+var amountOfApparitionsPerTypeInTheLastTen : Dictionary = {}
+var lastTenApparitions : Array[String] = []
+var amountOfApparitions : int = 0
 
 ## Returns the dictionary of glass sprites.
 func get_sprites() -> Dictionary:
@@ -19,15 +16,25 @@ func get_sprites() -> Dictionary:
 ## Returns a random glass key from the available sprites.
 func get_random_glass() -> String:
 	var keys = sprites.keys()
+	var returnType : String
 	
-	var returnType = keys[-1]
+	for type in sprites.keys():
+		amountOfApparitionsPerTypeInTheLastTen[type] = 0
+	for type in lastTenApparitions:
+		amountOfApparitionsPerTypeInTheLastTen[type] += 1
+	
+	returnType = keys[-1]
 	for type in keys.slice(0,-1):
-		var quot : float = (1.0+amountOfApparitionsPerType[type])/(keys.size()+sumOfApparitions)
+		var quot : float = (1.0+amountOfApparitionsPerTypeInTheLastTen[type])/(keys.size()+lastTenApparitions.size())
 		var probability : float = probabilityCurve.sample(quot)
+		print(type ,"  ",quot, "  ",probability ,"  ",1.0+amountOfApparitionsPerTypeInTheLastTen[type],"  ",keys.size()+lastTenApparitions.size())
 		if (randf() <= probability) :
 			returnType = type
 			break
 	
-	amountOfApparitionsPerType[returnType] += 1
-	sumOfApparitions += 1
+	amountOfApparitions += 1
+	if (amountOfApparitions >= 10):
+		lastTenApparitions.pop_front()
+	lastTenApparitions.push_back(returnType)
+	
 	return returnType
