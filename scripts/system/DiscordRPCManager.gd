75e8aea06166c4ebd957a55872d265d5a63ba4e3
Baptiste May <baptistemay@ik.me>
2026-07-08 18:35:26 +0200

    feat: disable discord rpc for android

--- scripts/system/DiscordRPCManager.gd
+++ scripts/system/DiscordRPCManager.gd
@@ -2,14 +2,21 @@ class_name DiscordRPCManager
 extends GameManagerInterface
 
 ## Manages Discord Rich Presence integration.
+## On Android, this module is disabled as the Discord RPC addon is not available.
 
 var _game_start_timestamp: int = 0
 var _pause_start_timestamp: int = 0
 var _is_paused: bool = false
 
+## Whether Discord RPC is available on the current platform.
+var _enabled: bool = false
+
 
 func init(p_game: GameManager) -> void:
 	super.init(p_game)
+	if OS.has_feature("android"):
+		return
+	_enabled = true
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	DiscordRPC.app_id = 1493718940147257365
 	DiscordRPC.large_image = "icon"
@@ -21,10 +28,14 @@ func init(p_game: GameManager) -> void:
 
 
 func _process(_delta: float) -> void:
+	if not _enabled:
+		return
 	DiscordRPC.run_callbacks()
 
 
 func set_main_menu() -> void:
+	if not _enabled:
+		return
 	_game_start_timestamp = 0
 	_is_paused = false
 	DiscordRPC.details = tr(&"RPC_MAIN_MENU")
@@ -35,16 +46,22 @@ func set_main_menu() -> void:
 
 
 func set_settings_menu() -> void:
+	if not _enabled:
+		return
 	DiscordRPC.details = tr(&"RPC_SETTINGS")
 	DiscordRPC.refresh()
 
 
 func set_credits_menu() -> void:
+	if not _enabled:
+		return
 	DiscordRPC.details = tr(&"RPC_CREDITS")
 	DiscordRPC.refresh()
 
 
 func set_playing(wave: int, is_night_mode: bool) -> void:
+	if not _enabled:
+		return
 	DiscordRPC.details = tr(&"RPC_GAME")
 	var mode_text := " (%s)" % tr(&"RPC_NIGHT_STATUS") if is_night_mode else ""
 	DiscordRPC.state = tr(&"RPC_WAVE_STATUS") % wave + mode_text
@@ -58,6 +75,8 @@ func set_playing(wave: int, is_night_mode: bool) -> void:
 
 
 func set_paused(is_paused: bool) -> void:
+	if not _enabled:
+		return
 	if is_paused:
 		if not _is_paused:
 			_pause_start_timestamp = int(Time.get_unix_time_from_system())
@@ -74,6 +93,8 @@ func set_paused(is_paused: bool) -> void:
 
 
 func set_game_over(wave: int) -> void:
+	if not _enabled:
+		return
 	DiscordRPC.details = tr(&"RPC_GAME_OVER_TITLE")
 	DiscordRPC.state = tr(&"RPC_GAME_OVER_DESCRIPTION") % wave
 	DiscordRPC.start_timestamp = _game_start_timestamp
