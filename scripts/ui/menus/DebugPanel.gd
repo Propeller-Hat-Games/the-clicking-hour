da77100dd96838b0b19b912bebccc755becce685
Baptiste May <baptistemay@ik.me>
2026-08-21 17:22:47 +0200

    :sparkles: feat: spawn entity in debug pannel (#33)

--- scripts/ui/menus/DebugPanel.gd
+++ scripts/ui/menus/DebugPanel.gd
@@ -7,6 +7,8 @@ var change_wave: int = 0
 
 func init(p_game: GameManager) -> void:
 	game = p_game
+	if OS.is_debug_build():
+		_setup_spawn_options()
 
 
 func _ready() -> void:
@@ -40,6 +42,7 @@ func _on_open_button_pressed() -> void:
 	$Content/Body/Informations/UnboadringGroup/CheckButton.button_pressed = (not (
 		SettingsManager.has_seen_onboarding
 	))
+	_setup_spawn_options()
 	$Content.visible = true
 	$OpenButton.visible = false
 	reset_size()
@@ -111,3 +114,122 @@ func update_has_seen_unboarding() -> void:
 
 func _on_unboarding_button_toggled(toggled_on: bool) -> void:
 	SettingsManager.has_seen_onboarding = not toggled_on
+
+
+func _setup_spawn_options() -> void:
+	var spawn_button: Button = $Content/Body/Spawn/Button
+	var entity_input: OptionButton = $Content/Body/Spawn/Entity/Input
+	var glass_input: OptionButton = $Content/Body/Spawn/Glass/Input
+
+	if not game:
+		spawn_button.disabled = true
+		return
+
+	if (
+		entity_input.item_count <= 1
+		and game.entities_manager
+		and not game.entities_manager.entity_scenes.is_empty()
+	):
+		entity_input.clear()
+		entity_input.add_item("Random")
+		entity_input.set_item_metadata(0, null)
+		for scene in game.entities_manager.entity_scenes:
+			var entity_name := _get_entity_name(scene)
+			var icon := _get_entity_icon(scene)
+			var idx := entity_input.item_count
+			if icon != null:
+				entity_input.add_icon_item(icon, entity_name)
+			else:
+				entity_input.add_item(entity_name)
+			entity_input.set_item_metadata(idx, scene)
+
+	if (
+		glass_input.item_count <= 1
+		and game.glass_manager
+		and not game.glass_manager.every_sprites.is_empty()
+	):
+		glass_input.clear()
+		glass_input.add_item("Random")
+		glass_input.set_item_metadata(0, "")
+		var glass_keys: Array[String] = []
+		glass_keys.assign(game.glass_manager.every_sprites.keys())
+		glass_keys.sort()
+		for key in glass_keys:
+			var idx := glass_input.item_count
+			var glass_sprite := game.glass_manager.get_glass_sprite(key)
+			var icon: Texture2D = glass_sprite.texture if glass_sprite else null
+			if icon != null:
+				glass_input.add_icon_item(icon, key.capitalize())
+			else:
+				glass_input.add_item(key.capitalize())
+			glass_input.set_item_metadata(idx, key)
+
+	spawn_button.disabled = (
+		game.spawn_area == null
+		or game.entities_manager == null
+		or game.glass_manager == null
+		or entity_input.item_count == 0
+		or glass_input.item_count == 0
+	)
+
+
+func _get_entity_name(scene: PackedScene) -> String:
+	var base_name := scene.resource_path.get_file().get_basename()
+	match base_name:
+		"entity":
+			return "Simple"
+		"hiding_entity":
+			return "Hiding"
+		"multi_click_entity":
+			return "MultiClick"
+		"teleport_entity":
+			return "Teleport"
+		_:
+			return base_name.capitalize()
+
+
+func _get_entity_icon(scene: PackedScene) -> Texture2D:
+	var base_name := scene.resource_path.get_file().get_basename()
+	var anim_name: StringName
+	match base_name:
+		"entity":
+			anim_name = &"normal_walk"
+		"hiding_entity":
+			anim_name = &"dig_walk"
+		"multi_click_entity":
+			anim_name = &"multiclick_walk"
+		"teleport_entity":
+			anim_name = &"tp_walk"
+		_:
+			anim_name = &"normal_walk"
+
+	var temp: Node = scene.instantiate()
+	var icon: Texture2D = null
+	var sprite: AnimatedSprite2D = temp.get_node_or_null("Sprite2D")
+	if sprite and sprite.sprite_frames and sprite.sprite_frames.has_animation(anim_name):
+		icon = sprite.sprite_frames.get_frame_texture(anim_name, 0)
+	temp.free()
+	return icon
+
+
+func _on_spawn_button_pressed() -> void:
+	if not game or not game.spawn_area or not game.entities_manager or not game.glass_manager:
+		return
+
+	var entity_input: OptionButton = $Content/Body/Spawn/Entity/Input
+	var glass_input: OptionButton = $Content/Body/Spawn/Glass/Input
+
+	if entity_input.item_count == 0 or glass_input.item_count == 0:
+		return
+
+	var selected_entity_idx := entity_input.selected
+	var entity_scene: PackedScene = null
+	if selected_entity_idx >= 0:
+		entity_scene = entity_input.get_item_metadata(selected_entity_idx)
+
+	var selected_glass_idx := glass_input.selected
+	var glass_type: String = ""
+	if selected_glass_idx >= 0:
+		glass_type = glass_input.get_item_metadata(selected_glass_idx)
+
+	game.spawn_area.spawn_entity(game, entity_scene, glass_type)
